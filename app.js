47fd190f4f1b5a6ce6f14adc6e9bea7b3fe05409
Kishan Mishra <98342089+kishankool@users.noreply.github.com>
2023-09-17 17:53:53 +0530

    made web socket chat app

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+// Serve static HTML file
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+// WebSocket server logic
+wss.on('connection', (ws) => {
+  console.log('A new client connected.');
+
+  // Event listener for incoming messages from clients
+  ws.on('message', (message) => {
+    console.log(`Received message: ${message}`);
+
+    // Broadcast the received message to all connected clients
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+
+  // Event listener for WebSocket disconnections
+  ws.on('close', () => {
+    console.log('A client disconnected.');
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
